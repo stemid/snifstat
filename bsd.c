d2bf84f058160f7857733f8627b0814f39c19ecb
Stefan Midjich <swehack@gmail.com>
2016-09-25 14:40:10 +0200

    fixed the hw address api.

--- bsd.c
+++ bsd.c
@@ -3,16 +3,13 @@
  * usr.sbin/rarpd/rarpd.c and reworked slightly to fit into this program.
  */
 
-/*
- * Lookup the ethernet address of the interface attached to the BPF
- * file descriptor 'fd'; return it in 'eaddr'.
- */
-u_char * lookup_addrs(char *ifname) {
-	struct ifaddrs *ifap, *ifa;
-	struct sockaddr_dl *sdl;
-	u_char eaddr[ETHER_ADDR_LEN];
-	struct if_addr *ia;
-	struct in_addr in;
+#include "snifstat.h"
+
+uint8_t * get_hw_address(char *ifname, int dflag) {
+	struct ifaddrs *ifap = NULL, *ifa = NULL;
+	struct sockaddr_dl *sdl = NULL;
+	uint8_t * eaddr = NULL;
+	struct if_addr *ia = NULL;
 	int found = 0;
 
 	if (getifaddrs(&ifap) != 0) {
@@ -26,57 +23,25 @@ u_char * lookup_addrs(char *ifname) {
 
 		sdl = (struct sockaddr_dl *) ifa->ifa_addr;
 		if (sdl->sdl_family == AF_LINK && sdl->sdl_type == IFT_ETHER && sdl->sdl_alen == 6) {
-			memcpy((caddr_t)eaddr, (caddr_t)LLADDR(sdl), 6);
+			/*memcpy((caddr_t)eaddr, (caddr_t)LLADDR(sdl), 6);*/
+			eaddr = (uint8_t*)LLADDR(sdl);
 
+			/* TODO: Remove debug code from API functions when finished. */
 			if (dflag) {
-				fprintf(stderr, "%s: %x:%x:%x:%x:%x:%x\n",
+				fprintf(stderr, "%s:MAC[%02X:%02X:%02X:%02X:%02X:%02X]\n",
 						ifa->ifa_name,
 						eaddr[0], eaddr[1], eaddr[2],
 						eaddr[3], eaddr[4], eaddr[5]);
 			}
 
 			found = 1;
-		} else if (sdl->sdl_family == AF_INET) {
-			ia = malloc(sizeof (struct if_addr));
-
-			if (ia == NULL) {
-				fprintf(stderr, "lookup_addrs: malloc: %s\n", strerror(errno));
-				return(-1);
-			}
-
-			ia->ia_next = NULL;
-			ia->ia_ipaddr = ((struct sockaddr_in *) ifa->ifa_addr)->sin_addr.s_addr;
-			ia->ia_netmask = ((struct sockaddr_in *) ifa->ifa_netmask)->sin_addr.s_addr;
-
-			/* If SIOCGIFNETMASK didn't work,
-				 figure out a mask from the IP
-				 address class. */
-			if (ia->ia_netmask == 0)
-				ia->ia_netmask = ipaddrtonetmask(ia->ia_ipaddr);
-
-			if (dflag) {
-				in.s_addr = ia->ia_ipaddr;
-				fprintf(stderr, "\t%s\n", inet_ntoa(in));
-			}
 		}
 	}
 
 	freeifaddrs(ifap);
 	if (!found && dflag) {
 		fprintf(stderr, "lookup_addrs: Never saw interface `%s'!", ifname);
-		return(-1);
 	}
-}
 
-/*
- * Get the netmask of an IP address.  This routine is used if
- * SIOCGIFNETMASK doesn't work.
- */
-u_int32_t ipaddrtonetmask(u_int32_t addr) {
-	if (IN_CLASSA(addr))
-		return IN_CLASSA_NET;
-	if (IN_CLASSB(addr))
-		return IN_CLASSB_NET;
-	if (IN_CLASSC(addr))
-		return IN_CLASSC_NET;
+	return eaddr;
 }
--- linux.c
+++ linux.c
@@ -1,5 +1,5 @@
 /* Functions to look up ethernet address on Linux systems.
- */
+*/
 
 #include "snifstat.h"
 
@@ -16,33 +16,26 @@ uint8_t * get_hw_address(char *ifname, int dflag) {
 
 	if(getifaddrs(&ifa) == -1) {
 		perror("getifaddrs: ");
-		return(-1);
+		return(NULL);
 	}
 
 	if(sd < 0) {
 		freeifaddrs(ifa);
-		return(-1);
+		return(NULL);
 	}
 
 	for(;ifa;ifa = ifa->ifa_next) {
-		/* Find if matching the ifname. */
 		if(ifa->ifa_data != 0) {
 			continue;
 		}
+
+		/* Find if matching the ifname. */
 		if(strncmp(ifname, ifa->ifa_name, sizeof(*ifname)) == 0) {
-			strncpy(req.ifr_name, ifa->ifa_name, sizeof(ifa->ifa_name));
+			strncpy(req.ifr_name, ifa->ifa_name, IFNAMSIZ);
 			if(ioctl(sd, SIOCGIFHWADDR, &req ) != -1 ) {
 				mac = (uint8_t*)req.ifr_ifru.ifru_hwaddr.sa_data;
-
-				if(dflag) {
-					fprintf(stderr, "%s:MAC[%02X:%02X:%02X:%02X:%02X:%02X]\n",
-							ifa->ifa_name,
-							mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-				}
+				break;
 			}
-
-			printf("Found if %s\n", ifa->ifa_name);
-			break;
 		}
 	}
 

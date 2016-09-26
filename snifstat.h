033a9baf2838edc06191d9818ce5d5bdf3bfbafc
Stefan Midjich <swehack@gmail.com>
2016-09-26 08:04:08 +0200

    need to find an alternative to MAX_ADDR_LEN to use. also added prototypes.

--- snifstat.h
+++ snifstat.h
@@ -43,13 +43,24 @@ uint8_t * get_hw_address(char *, int);
 /* Linux MAX_ADDR_LEN */
 #include <linux/netdevice.h>
 
+/* ether_header */
+#include <net/ethernet.h>
+
 /* IFNAMSIZ */
 #include <linux/if.h>
-
-#define ETHER_ADDR_LEN MAX_ADDR_LEN
 
 uint8_t * get_hw_address(char *, int);
 #endif
 
 /* duh -lpcap */
 #include <pcap.h>
+
+void usage(const char *);
+void break_capture(int);
+void cleanup_capture(int);
+unsigned short get_windowsize(void);
+int hwaddrscmp(uint8_t *, uint8_t *);
+void output_header(char *);
+void output_data(double, double);
+void capture_callback(u_char *, const struct pcap_pkthdr *, const u_char *);
+

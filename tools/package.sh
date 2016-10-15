0ae5376bf15db3470a6cbfa9a9f45f7553caadb0
Stefan Midjich <swehack@gmail.com>
2016-10-15 11:40:47 +0200

    addig proper wrksrc for fbsd.

--- tools/package.sh
+++ tools/package.sh
@@ -11,37 +11,38 @@ test -z $1 && exit 1
 
 # RPM
 if [ $1 = 'RPM' ]; then
-	fpm -s dir -t rpm -n $name -v $version -d libpcap -m "$contact" --description "$descr" $files
+    fpm -s dir -t rpm -n $name -v $version -d libpcap -m "$contact" --description "$descr" $files
 fi
 
 # Deb
 if [ $1 = 'Debian' ]; then
-	fpm -s dir -t deb -n $name -v $version -d libpcap0.8 -m "$contact" --description "$descr" $files
+    fpm -s dir -t deb -n $name -v $version -d libpcap0.8 -m "$contact" --description "$descr" $files
 fi
 
 # FreeBSD ports
 if [ $1 = 'FreeBSD' ]; then
-	fbsd_plist_file=FreeBSD/pkg-plist
-	fbsd_makefile=FreeBSD/Makefile
-	fbsd_header='$FreeBSD$'
-	fbsd_sites="https://github.com/stemid/snifstat/archive/"
-	fbsd_categories=net
+    fbsd_plist_file=FreeBSD/pkg-plist
+    fbsd_makefile=FreeBSD/Makefile
+    fbsd_header='$FreeBSD$'
+    fbsd_sites="https://github.com/stemid/snifstat/archive/"
+    fbsd_categories=net
     filename="${name}-${version}.tar.gz"
     fbsd_remote_file="${fbsd_sites}/${filename}"
     fbsd_distinfo=FreeBSD/distinfo
     fbsd_pkg_descr=FreeBSD/pkg-descr
     fbsd_files='/usr/local/bin/snifstat /usr/local/share/man/man1/snifstat.1'
     fbsd_symlink="${name}-${version}"
+    fbsd_wrksrc='${WRKDIR}/${PORTNAME}-${PORTNAME}-${PORTVERSION}'
 
-	export name version contact descr fbsd_sites fbsd_categories fbsd_header
+    export name version contact descr fbsd_sites fbsd_categories fbsd_header fbsd_wrksrc
 
-	:> $fbsd_plist_file
-	for file in $fbsd_files; do
-		echo ${file/\/} >> $fbsd_plist_file
-	done
+    :> $fbsd_plist_file
+    for file in $fbsd_files; do
+        echo ${file/\/} >> $fbsd_plist_file
+    done
 
-	:> $fbsd_makefile
-	envsubst < "${fbsd_makefile}.in" > $fbsd_makefile
+    :> $fbsd_makefile
+    envsubst < "${fbsd_makefile}.in" > $fbsd_makefile
 
     # Create simple pkg-descr
     echo $descr > $fbsd_pkg_descr
@@ -56,7 +57,7 @@ if [ $1 = 'FreeBSD' ]; then
     echo "SHA256 (${filename}) = $fbsd_checksum" > $fbsd_distinfo
     echo "SIZE (${filename}) = $fbsd_size" >> $fbsd_distinfo
 
-	# Now make tarball
+    # Now make tarball
     ln -s FreeBSD "$fbsd_symlink"
     tar -cvzf "${name}-${version}.tar.gz" --exclude='Makefile.in' "${fbsd_symlink}"/*
 
